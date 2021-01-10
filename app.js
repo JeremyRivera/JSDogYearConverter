d600f612c52f0aceba9a66a4370a950b188c9cff
JeremyRivera <jeremyrivera23@yahoo.com>
2021-01-10 13:32:47 -0500

    Create app.js

---
+++ app.js
@@ -0,0 +1,15 @@
+// Dog Years Converter
+const myAge = 25;
+const myName = 'Jeremy'
+
+let earlyYears = 2;
+earlyYears *= 10.5;
+
+let laterYears = myAge - 2;
+laterYears *= 4;
+
+const myAgeInDogYears = earlyYears + laterYears;
+
+'Jeremy'.toLowerCase();
+
+console.log(`My name is ${myName}. I am ${myAge} years old in human years which is DOG AGE years old in dog years.`)
